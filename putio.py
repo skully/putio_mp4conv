854afab460fec926f2d700b948d72853be56b767
skully <alexa.vass@gmail.com>
2018-05-08 21:44:14 +0200

    finished script, also supports webm

--- putio.py
+++ putio.py
@@ -1,18 +1,17 @@
-import requests, json
+import requests, json, os
 
 
 def config():
-    
     confDict = dict()
     try:
-        print "Config file found!"
+        print("Config file found!")
         with open(".config", "r") as confFile:
             for confLine in confFile:
                 listOfConf = confLine.replace(" ", "").split(":",1)
                 confDict.update({listOfConf[0]:listOfConf[1]})
 
     except IOError:
-        print "No .config file! Let's make one!"
+        print("No .config file! Let's make one!")
         confFile = open(".config", "w")
 
         oAuthToken = raw_input("OAuthToken: ")
@@ -24,10 +23,6 @@ def config():
     return confDict
 
 
-
-
-
-
 def get_filelist(confDict, id):
     payload = {'oauth_token':confDict['oauth']}
 
@@ -37,6 +32,64 @@ def get_filelist(confDict, id):
         payload.update({'parent_id': id})
         result = requests.get("https://api.put.io/v2/files/list", params=payload)
 
+    parsed = json.loads(result.text)
+    return parsed
+
+
+def get_infos(confDict,id):
+    payload = {'oauth_token':confDict['oauth']}
+    result = requests.get("https://api.put.io/v2/files/"+ str(id) , params=payload)
+    parsed = json.loads(result.text)
+    if parsed['status'] ==  'OK':
+        return parsed['file']
+    else:
+        return "ERR"
+
+
+def download_file(confDict, id, path = "."):
+    payload = {'oauth_token':confDict['oauth']}
+    url = "https://api.put.io/v2/files/" + str(id) + "/download"
+
+    try:
+        local_file = get_infos(confDict,id)
+    except requests.exceptions.ConnectionError:
+        return -1
+    
+    local_filename = local_file['name'] 
+    
+    if not os.path.exists(path):
+            os.makedirs(path)
+
+    r = requests.get(url, params = payload,stream = True)
+    with open((path+"/"+local_filename), 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: 
+                f.write(chunk)
+                f.flush()
+    
+    return local_filename
+
+def upload_file(confDict, path_to_file = ".", parent_id=0): 
+    payload = {'oauth_token' : confDict['oauth']}
+    payload["parent_id"] = parent_id
+
+    try:
+        files = {'file' : open(path_to_file,'rb')}
+        url = 'https://upload.put.io/v2/files/upload'
+        
+        result = requests.post(url, params=payload, files= files )
+
+        files['file'].close
+        parsed = json.loads(result.text)
+        return parsed
+    except NameError:
+        return None    
+
+def move_file(confDict, file_id, parent_id):
+    url = 'https://api.put.io/v2/files/move'
+    payload = {'oauth_token' : confDict['oauth']}
+    payload.update( {'file_ids': str(file_id), 'parent_id': str(parent_id)})
+    result = requests.post(url, data=payload)
     parsed = json.loads(result.text)
     return parsed
 
--- putio_mp4conv.py
+++ putio_mp4conv.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
 import putio
+from subprocess import call
+import os
 
 dl_dirname = 'youtube'
 ul_dirname = 'ytmp3'
+done_dirname = 'ytconverted'
 
+#solve this by search api request and full text match
 def get_file_id(name):
     #it's in the root!
     root = putio.get_filelist(confdict, 0)
@@ -21,31 +25,71 @@ def get_file_list():
     return dlfileids
 
 
-def convert_every_file(fileList=[]):
-    #ulid = get_file_id(ul_dirname)
-    for i in fileList:
-        download_mp4()
-        convert_mp4_to_mp3()
-        upload_mp3()
-        cleanup()
+def convert_and_upload_every_file(fileList=[]):
+    ulid = get_file_id(ul_dirname)
+    for id in fileList:
+        filename = download_mp4(id)
+        print("file: "+filename)
+        mp3_filepath = convert_mp4_to_mp3(filename)
+        upload_mp3(mp3_filepath,ulid)
+        cleanup(id, mp3_filepath)
+        
 
-def download_mp4():
-    pass
+def download_mp4(id):
+    return putio.download_file(confdict, id, "./working")
 
-def convert_mp4_to_mp3():
-    pass
 
-def upload_mp3():
-    pass
+def convert_mp4_to_mp3(filename_with_extension):
+    indir = "./working"
+    outdir = "./outdir"
+    print(filename_with_extension[0:-4]) 
+    if filename_with_extension[-4:] == ".mp4":
+        filename = filename_with_extension[0:-4]
+        print("-- converting {0}/{2}.mp4 to {1}/{2}.mp3 --".format(indir, outdir, filename))
+        call(["mplayer", "-novideo", "-nocorrect-pts", "-ao", "pcm:waveheader", indir + "/" + filename +".mp4"])
 
-def cleanup():
-    pass
+        if not os.path.exists(outdir):   
+            os.makedirs(outdir)
+
+        call(["lame", "-v", "audiodump.wav", outdir + "/" + filename + ".mp3"])
+        os.remove("audiodump.wav")
+        os.remove(indir + "/" + filename_with_extension)
+        print(outdir + "/" + filename + ".mp3"+"mp4")
+        return outdir + "/" + filename + ".mp3"
+
+    elif filename_with_extension[-5:] == ".webm": 
+        filename = filename_with_extension[0:-5]
+        call(["ffmpeg", "-i", indir + "/" + filename_with_extension, "-vn", "-ab", "128k", "-ar", "44100",
+                "-y", outdir + "/" + filename + ".mp3"])
+        #os.remove(indir + "/" + filename_with_extension)
+        print(outdir + "/" + filename + ".mp3"+"webm")
+        return outdir + "/" + filename + ".mp3"
+
+
+
+def upload_mp3(mp3_path, upload_id):
+    putio.upload_file(confdict, mp3_path, upload_id)
+
+
+def cleanup(id, mp3_path):
+    move_mp4_in_putio(id)
+    delete_mp3_on_host(mp3_path)
+
+
+def move_mp4_in_putio(id):
+    done_id = get_file_id(done_dirname)
+    putio.move_file(confdict, id, done_id)
+
+
+def delete_mp3_on_host(mp3_path):
+    os.remove(mp3_path)
+
 
 def main():
     global confdict 
     confdict = putio.config()
     fileList = get_file_list()
-    convert_every_file(fileList)
+    convert_and_upload_every_file(fileList)
 
 
 
